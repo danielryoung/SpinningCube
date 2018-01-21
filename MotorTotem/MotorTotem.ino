b9d1e2b6c2bab36648fe8cd927c629e9814d999e
Daniel Young -L <danielryoung@gmail.com>
2018-01-21 01:00:04 -0800

    Add initial pseudo code

---
+++ MotorTotem/MotorTotem.ino
@@ -0,0 +1,110 @@
+#include <Servo.h>
+#include <FastLED.h>
+
+
+#define DATAPIN MOSI //D7
+#define CLOCKPIN SCK //D5
+#define ESCPIN D1
+// BTSerial on RT TX
+
+//FASTLEDSETUP
+#define NUM_LEDS 24
+#define CHIPSET APA102
+#define COLOR_ORDER BGR
+#define BRIGHTNESS 100
+
+
+//this is the pot value diff that needs to exist before we make change to speed.
+#define SPEEDDELTA 5
+#define MTRSPDINTERVAL 50
+
+// attach stuff
+Servo esc;
+elapsedMillis sinceMotorAdj;
+
+//globals
+
+
+//this is the Motor speed that motor is moving at, will change
+int currentMotorSpeed = 0;
+
+//apply some smoothing by taking what we want speed to be and moving to it.
+int desiredMotorSpeed = 0; // pot1 value
+
+int desiredFlashRate = 0; //pot2 value
+
+String reading;
+
+void setup(){
+  delay(10);
+  esc.attach(ESCPIN);
+  Serial.begin(38400);
+
+  delay(3000);
+ FastLED.addLeds<CHIPSET, DATAPIN, CLOCKPIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+ FastLED.setBrightness(BRIGHTNESS);
+ 
+ //Metro motorTransition = Metro(MTRSPDINTERVAL);
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ //maybe timer here also. get readings and parse only occasionally
+  parseReading();
+
+  if (sinceMotorAdjust >= MTRSPDINTERVAL ) {
+  adjustMotorSpeed();
+  }
+
+  if(colorneedstochange){do it};
+
+  
+  flash timer here
+  flash;
+  
+
+}
+
+
+//void startupMotor(){}
+
+//void shutdownMotor(){}
+
+//void setMotorSpeed(spd){
+
+
+  // if pot is different by SPEEDDELTA then transition to new speed
+ //return yes to move, return no to not chagne
+ 
+ // if (compareReading(spd)) {
+ //transitionSpeed(spd);
+};
+
+ 
+  
+void parseReading(){
+
+   if(Serial.available() > 0) {
+    reading = Serial.read();
+ 
+    desiredMotorSpeed = strtok(reading, ','); //pot1 first val comma sep
+    desiredFlashRate = strtok();
+    
+   }
+}
+
+void adjustMotorSpeed() {
+//this gets called by timer callback
+  if(desiredMotorSpeed > currentMotorSpeed){
+      currentMotorSpeed++;
+  }
+    else if(desiredMotorSpeed < currentMotorSpeed){
+      currentMotorSpeed--;
+    }
+    else{}
+} 
+  
+
+
