fc3746e39bf517e36f663ff0983f02f06806d2b4
Daniel Young -L <danielryoung@gmail.com>
2018-02-11 17:48:50 -0800

    add broken color cycler

--- MotorTotem/MotorTotem.ino
+++ MotorTotem/MotorTotem.ino
@@ -1,76 +1,209 @@
-#include <Servo.h>
 #include <FastLED.h>
+#include <Servo.h>
 
+#define HWSERIAL Serial1
+ 
+Servo esc;
+//SoftwareSerial BTSerial(5,6);
 
-#define DATAPIN MOSI //D7
-#define CLOCKPIN SCK //D5
-#define ESCPIN D1
-// BTSerial on RT TX
-
-//FASTLEDSETUP
-#define NUM_LEDS 24
-#define CHIPSET APA102
+#define DATA_PIN     11
+#define CLOCK_PIN     14
 #define COLOR_ORDER BGR
-#define BRIGHTNESS 100
+#define CHIPSET     APA102
+#define NUM_LEDS    24
+#define LED_GROUP 6
+#define ESC_PIN 20
+#define RX_PIN 0
+#define TX_PIN 1
+
+
+#define BRIGHTNESS  100
+#define FRAMES_PER_SECOND 10
 
 
 //this is the pot value diff that needs to exist before we make change to speed.
-#define SPEEDDELTA 5
+#define SPEEDDELTA 10
 #define MTRSPDINTERVAL 50
 
+
+// Fire2012 by Mark Kriegsman, July 2012
+// as part of "Five Elements" shown here: http://youtu.be/knWiGsmgycY
+//// 
+// This basic one-dimensional 'fire' simulation works roughly as follows:
+// There's a underlying array of 'heat' cells, that model the temperature
+// at each point along the line.  Every cycle through the simulation, 
+// four steps are performed:
+//  1) All cells cool down a little bit, losing heat to the air
+//  2) The heat from each cell drifts 'up' and diffuses a little
+//  3) Sometimes randomly new 'sparks' of heat are added at the bottom
+//  4) The heat from each cell is rendered as a color into the leds array
+//     The heat-to-color mapping uses a black-body radiation approximation.
+//
+// Temperature is in arbitrary units from 0 (cold black) to 255 (white hot).
+//
+// This simulation scales it self a bit depending on NUM_LEDS; it should look
+// "OK" on anywhere from 20 to 100 LEDs without too much tweaking. 
+//
+// I recommend running this simulation at anywhere from 30-100 frames per second,
+// meaning an interframe delay of about 10-35 milliseconds.
+//
+// Looks best on a high-density LED setup (60+ pixels/meter).
+//
+//
+// There are two main parameters you can play with to control the look and
+// feel of your fire: COOLING (used in step 1 above), and SPARKING (used
+// in step 3 above).
+//
+// COOLING: How much does the air cool as it rises?
+// Less cooling = taller flames.  More cooling = shorter flames.
+// Default 50, suggested range 20-100 
+#define COOLING  55
+
+// SPARKING: What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire.  Lower chance = more flickery fire.
+// Default 120, suggested range 50-200.
+#define SPARKING 80
+bool gReverseDirection = false;
+
+
+
 // attach stuff
-Servo esc;
 elapsedMillis sinceMotorAdj;
-elapsedMicros sinceFlash;
+elapsedMillis sinceFlash;
+elapsedMicros offFlash;
 
+CRGB leds[NUM_LEDS];
 //globals
-
+unsigned int flashTime = 3000;
 
 //this is the Motor speed that motor is moving at, will change
-int currentMotorSpeed = 0;
+int currentMotorSpeed = 40;
 
 //apply some smoothing by taking what we want speed to be and moving to it.
 int desiredMotorSpeed = 0; // pot1 value
 
-int flashRate = 0; //pot2 value
+unsigned int flashRate = 200; //pot2 value
 
-String reading;
+byte reading = 0;
 
-void setup(){
-  delay(10);
-  esc.attach(ESCPIN);
-  Serial.begin(38400);
+void setup() {
+  delay(3000); // sanity delay
+  FastLED.addLeds<CHIPSET,DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness( BRIGHTNESS );
+  //add something to black out leds here
+  esc.attach(ESC_PIN);
+  HWSERIAL.setRX(RX_PIN);
+  HWSERIAL.setTX(TX_PIN);
+  HWSERIAL.begin(38400);
+ // BTSerial.begin(38400);
+}
+void Fire2012()
+{
+// Array of temperature readings at each simulation cell
+  static byte heat[NUM_LEDS];
 
-  delay(3000);
- FastLED.addLeds<CHIPSET, DATAPIN, CLOCKPIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
- FastLED.setBrightness(BRIGHTNESS);
- 
- //Metro motorTransition = Metro(MTRSPDINTERVAL);
- 
+  // Step 1.  Cool down every cell a little
+    for( int i = 0; i < NUM_LEDS; i++) {
+      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    }
+  
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for( int k= NUM_LEDS - 1; k >= 2; k--) {
+      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    }
+    
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if( random8() < SPARKING ) {
+      int y = random8(7);
+      heat[y] = qadd8( heat[y], random8(160,255) );
+    }
+
+    // Step 4.  Map from heat cells to LED colors
+    for( int j = 0; j < NUM_LEDS; j++) {
+      CRGB color = HeatColor( heat[j]);
+      int pixelnumber;
+      if( gReverseDirection ) {
+        pixelnumber = (NUM_LEDS-1) - j;
+      } else {
+        pixelnumber = j;
+      }
+      leds[pixelnumber] = color;
+    }
 }
 
+void parseReading(){
+       
+   if(HWSERIAL.available()>0) {
+    reading = HWSERIAL.read();
+ 
+    desiredMotorSpeed = reading; //pot1 first val comma sep
+    flashRate = map(reading,0,180,200,60);
+    flashTime = 12000;
+    //TODO Calculate flash time from flashrate in microseconds
+    
+   }
+  esc.write(reading);
+}
+ 
+void adjustMotorSpeed() {
+//this gets called by timer callback
+  if(desiredMotorSpeed > currentMotorSpeed){
+      currentMotorSpeed++;
+  }
+    else if(desiredMotorSpeed < currentMotorSpeed){
+      currentMotorSpeed--;
+    }
+    else{}
+} 
+
+  
 void loop() {
   // put your main code here, to run repeatedly:
 
  //maybe timer here also. get readings and parse only occasionally
   parseReading();
 
-  if (sinceMotorAdjust >= MTRSPDINTERVAL ) {
+  if (sinceMotorAdj >= MTRSPDINTERVAL ) {
   adjustMotorSpeed();
   }
 
 //see if its time to turn on then wait to turn off again
+
+if (reading > 30) {
+  
   if(sinceFlash > flashRate){
-    leds[2] = CRGB:Green;
+    CRGB color1 = CRGB::Red;
+    CRGB color2 = CRGB::Blue;
+    for( int i = 0; i < NUM_LEDS; i++) {
+       int g = ((LED_GROUP + i) - (i%LED_GROUP))/ LED_GROUP;
+
+       switch (g) {
+        case 1: leds[i] = color1;
+        case 2: leds[i] = color2;
+        case 3: leds[i] = color1;
+        case 4: leds[i] = color2;
+        }
+ 
+    }
     sinceFlash = 0;
     offFlash = 0;
-    };
+  }
   if (offFlash > flashTime){
-    leds[2] = CRGB;Red;
-    sinceFlash = 0;
+    CRGB color = CRGB::Cyan;
+   for( int i = 0; i < NUM_LEDS; i++) {
+   
+    leds[i] = color;
+      }
+    //sinceFlash = 0;
   } 
-
+}else {
+  Fire2012();
+  FastLED.delay(1000 / FRAMES_PER_SECOND);
 }
+  FastLED.show(); // display this frame
+  //FastLED.delay(1000 / FRAMES_PER_SECOND);
+}
+
 
 
 //void startupMotor(){}
@@ -85,33 +218,8 @@ void loop() {
  
  // if (compareReading(spd)) {
  //transitionSpeed(spd);
-};
 
- 
-  
-void parseReading(){
 
-   if(Serial.available() > 0) {
-    reading = Serial.read();
- 
-    desiredMotorSpeed = strtok(reading, ','); //pot1 first val comma sep
-    flashRate = strtok();
-    flashTime = 3000;
-    //TODO Calculate flash time from flashrate in microseconds
-    
-   }
-}
 
-void adjustMotorSpeed() {
-//this gets called by timer callback
-  if(desiredMotorSpeed > currentMotorSpeed){
-      currentMotorSpeed++;
-  }
-    else if(desiredMotorSpeed < currentMotorSpeed){
-      currentMotorSpeed--;
-    }
-    else{}
-} 
-  
 
 
