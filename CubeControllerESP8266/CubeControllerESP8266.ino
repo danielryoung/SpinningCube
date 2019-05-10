8a7c67365aba06b01b45cfc1290033ddaf68ca62
Daniel Young <danielryoung@gmail.com>
2019-05-10 09:45:51 -0700

    some local feature commit

--- CubeControllerESP8266/CubeControllerESP8266.ino
+++ CubeControllerESP8266/CubeControllerESP8266.ino
@@ -252,10 +252,11 @@ void toggleMotor(){
 // This will start up the motor or shut it down smoothly
   if (motorRunning){
     Serial.println("Stopping Motor...");
-    // get the motor running.
+    // get the motor running.  
     motorRunning = false;
    }  else {
     Serial.println("Starting Motor...");
+    // stop the motor!
     motorRunning = true;
    }
 }
